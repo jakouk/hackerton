3ffcdfe13b30f181bdf8f8948355d45f8d8b5b50
jakouk <jakoriaty@gmail.com>
2016-11-03 21:18:12 +0900

    update spaceOdyssey

--- SpaceOdyssey/SpaceOdyssey/GameScene.m
+++ SpaceOdyssey/SpaceOdyssey/GameScene.m
@@ -29,16 +29,16 @@ typedef NS_ENUM(NSInteger,Game) {
     _movingGameObject = [SKNode node];
     [self addChild:_movingGameObject];
     
-    SKTexture* backgroundTexture = [SKTexture textureWithImageNamed:@"bg"];
-    SKAction *moveBackground = [SKAction moveByX:0 y:-backgroundTexture.size.width duration:12];
-    SKAction *replaceBackground = [SKAction moveByX:0 y:backgroundTexture.size.width duration:0];
+    SKTexture* backgroundTexture = [SKTexture textureWithImageNamed:@"spaceBackground"];
+    SKAction *moveBackground = [SKAction moveByX:0 y:-backgroundTexture.size.height duration:12];
+    SKAction *replaceBackground = [SKAction moveByX:0 y:backgroundTexture.size.height duration:0];
     SKAction *backgroundSequence = [SKAction sequence:@[moveBackground,replaceBackground]];
     SKAction *moveBackgroundForever = [SKAction repeatActionForever:backgroundSequence];
     
     for (NSInteger i = 0; i < 2; i+=1) {
         _background = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
-        _background.position = CGPointMake(backgroundTexture.size.width/2 +backgroundTexture.size.width * i, self.frame.size.height/2);
-        [_background setSize:CGSizeMake(self.view.frame.size.height, self.view.frame.size.height)];
+        _background.position = CGPointMake(backgroundTexture.size.width/2, backgroundTexture.size.height/2 + backgroundTexture.size.height * i);
+        [_background setSize:CGSizeMake(self.frame.size.width, self.frame.size.height)];
         [_background runAction:moveBackgroundForever];
         [_movingGameObject addChild:_background];
     }
